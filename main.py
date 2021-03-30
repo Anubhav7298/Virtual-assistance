41d72b9e145977c452925df05ebfc723a0227ad8
Anubhav7298 <61357115+Anubhav7298@users.noreply.github.com>
2021-03-30 22:32:39 +0530

    added some comments

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ engine = pyttsx3.init()
 engine.say("speak now")
 engine.runAndWait()
 
+# this function is used to execute the function after extracting text from it
+
 
 def fun(url1, text1):
     try:
@@ -23,6 +25,8 @@ def fun(url1, text1):
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
+# to listen and convert the audio to text
+
 
 with sr.Microphone() as source:
     r1.adjust_for_ambient_noise(source)
@@ -30,11 +34,14 @@ with sr.Microphone() as source:
     audio = r1.listen(source)
     text = r1.recognize_google(audio)
 
+# if search is in the voice then the browser is open and the text is searched in google
 
 if 'search' in text:
     text = text[7:]
     url = f"https://www.google.com/search?q={text}"
     fun(url, text)
+
+# if play is in the voice then the browser is open and the text is searched in youtube
 
 if 'play' in text:
     text = text[5:]
